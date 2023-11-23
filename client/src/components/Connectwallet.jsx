f1386c7fc69cf09d12ff5ccf83e8455f2060f09b
Mustakimkhan <mustakimkhan1111176@gmail.com>
2023-11-23 14:06:55 +0000

    adding chain connection

--- client/src/components/Connectwallet.jsx
+++ client/src/components/Connectwallet.jsx
@@ -5,6 +5,35 @@ import { contractAddress, ABI } from '../contract'
 export const Connectwallet = ({ setSmartContract, walletAddress, setWalletAddress }) => {
 
     const updateCurrentWalletAddress = async () => {
+        try {
+            await ethereum.request({
+                method: 'wallet_switchEthereumChain',
+                params: [{ chainId: '0x13881' }],
+            });
+        } catch (switchError) {
+            if (switchError.code === 4902) {
+                try {
+                    await ethereum.request({
+                        method: 'wallet_addEthereumChain',
+                        params: [
+                            {
+                                chainId: '80001',
+                                chainName: 'Mumbai Testnet',
+                                rpcUrls: ['https://rpc-mumbai.maticvigil.com'],
+                                nativeCurrency: {
+                                    name: "MATIC",
+                                    symbol: "MATIC",
+                                    decimals: 18
+                                },
+                                blockExplorerUrls: ["https://polygonscan.com/"]
+                            },
+                        ],
+                    });
+                } catch (addError) {
+                    toast.error("please try again")
+                }
+            }
+        }
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
             .catch((err) => {
                 if (err.code === 4001) {
@@ -25,8 +54,8 @@ export const Connectwallet = ({ setSmartContract, walletAddress, setWalletAddres
             toast.error("please try again")
         }
 
-
     }
+
     const connectwallet = async () => {
         if (window.ethereum) {
             updateCurrentWalletAddress()
